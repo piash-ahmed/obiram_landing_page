fc7ebe917b80293ec9d1eb78ce9ee485ac83f3f7
piash-ahmed <aryanpiash420@gmail.com>
2025-12-23 21:57:10 +0600

    Initialized and added Navbar and Hero section

--- app/page.js
+++ app/page.js
@@ -4,11 +4,10 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <main>
-    <section className="bg-[#fb4712]">
-      <Navbar />
-      <Hero />
-    </section>
-      
+      <section className="bg-[#fb4712]">
+        <Navbar />
+        <Hero />
+      </section>
     </main>
   );
 }
