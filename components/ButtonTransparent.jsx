0a0c4724ccb045340ed372a6e3bd48f330df8e20
piash-ahmed <aryanpiash420@gmail.com>
2025-12-23 21:48:01 +0600

    recheck changes

--- components/ButtonTransparent.jsx
+++ components/ButtonTransparent.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function ButtonTransparent({value}) {
   return (
-    <button className="py-2 px-4 bg-[#f14d1d] border border-[#f68260] text-white font-normal rounded-lg ml-5 text-[12px] font-bold">
+    <button className="py-2 px-4 bg-[#f14d1d] border border-[#f68260] text-white font-normal rounded-lg ml-4 text-[12px] font-bold">
       <Image src="/images/youtube.png" alt="right arrow" width={24} height={24} className="inline-block mr-2"
        />
         {value}
